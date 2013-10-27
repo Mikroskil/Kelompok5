cc548f8e92ca59a2713aed12ac18b5851daf2b02
Susanti Amelia <wan6_Suam3@yahoo.com>
2013-10-27 13:55:12 +0700

    commit1

---
+++ template/index.php
@@ -0,0 +1,21 @@
+<html>
+	<head>
+		<title>Q&A</title>
+		<link rel="stylesheet" type="text/css" href="../assets/css/global.css" />
+	</head>
+	<body>
+		<?php include_once 'header.html'; ?>
+		<br class="break" />
+		<?php include_once 'menu.html'; ?>
+		<div class="container">
+			<h2>Recent Questions</h2>
+			<div id="contain">
+				<p>Item One</p>
+				<p>Item Two</p>
+				<p>Item Three</p>
+			</div>
+		</div>
+		<br class="break" />
+		<?php include_once 'footer.html'; ?>
+	</body>
+</html>
