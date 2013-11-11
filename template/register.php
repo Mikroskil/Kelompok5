26676b28bcf0db2eb5e6e15ff4e880c649c709d2
andyciwe <acw_chanel@yahoo.com>
2013-11-11 23:03:35 +0700

    regex utk register

--- template/register.php
+++ template/register.php
@@ -2,25 +2,74 @@
 	<head>
 		<title>Register</title>
 		<link rel="stylesheet" type="text/css" href="../assets/css/global.css" />
+		
 	</head>
 	<body>
 		<?php include_once 'header.html'; ?>
 		<br class="break" />
+		
 		<?php include_once 'menu.html'; ?>
+		
 		<div class="container">
+		
 			<h2>Create Account</h2>
 			<div id="contain">
+			<?php
+			$error = "&nbsp";
+			if (isset($_POST["submit"]))
+			{
+				if (!preg_match("/^[a-zA-Z]{1}[a-zA-Z0-9\.\-\_]+$/",$_POST["username"]))
+					$error = "Id kosong atau salah!";
+				else if (!preg_match("/^[a-zA-Z]+$/",$_POST["name"]))
+					$error = "Nama kosong atau salah!";
+				else if (!preg_match("/^[a-zA-Z0-9]+([a-zA-Z0-9_.]+)[@]{1}[a-zA-Z]+(([.]{1}[a-zA-Z]{2,4}){1,})/",$_POST["email"]))
+					$error = "Email kosong atau salah!";
+				else if (!preg_match("/^[a-zA-Z0-9]{4,12}+$/",$_POST["password"]))
+					$error = "Password salah atau kurang dari 4 karakter!";
+				else
+				{
+					$data = Array();
+					$cek = true;
+					
+					$connect = mysql_connect("localhost","root","");
+					mysql_select_db("easyask",$connect);
+					$tabel = mysql_query("SELECT * FROM user");
+					
+					$username = $_POST["username"];
+					while ($row = mysql_fetch_array($tabel))
+					{
+						if ($row['username'] == $_POST["username"])
+						{
+							$error = "Id telah terpakai!";
+							$cek = false;
+						}
+					}
+					
+					if ($cek)
+					{
+						$nama = $_POST["nama"];
+						$email = $_POST["email"];
+						$pass = $_POST["pass"];
+					
+						$insert = mysql_query("INSERT INTO user (username, password,nama,email)
+						VALUES ('$username' , '$password' , '$nama' , '$email') " , $connect);
+					}
+					mysql_close($connect);
+				}
+				echo $error;
+			}
+		?>
 				<form action="" method="post">
 				<table>
-				<tr>
-					<td>Full Name</td>
-					<td> : </td>
-					<td><input type="text" name="name" size="30"></td>
-				</tr>
 				<tr>
 					<td>Username</td>
 					<td> : </td>
 					<td><input type="text" name="username" size="15"></td>
+				</tr>
+				<tr>
+					<td>Full Name</td>
+					<td> : </td>
+					<td><input type="text" name="name" size="30"></td>
 				</tr>
 				<tr>
 					<td>Password</td>
