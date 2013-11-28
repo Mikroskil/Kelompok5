c3186b46b5c205fd64078b58ee65bb78eeae1249
jodiesetiawan <jodiesetiawan@gmail.com>
2013-11-27 23:37:56 -1200

    Posting jawaban

--- sql/answer.php
+++ sql/answer.php
@@ -6,7 +6,7 @@ function postAnswer($jb, $doodle, $id_pertanyaan, $id_user)
 {
 	global $mysql;
 
-	$jb = mysql_escape_string($isi);
+	$jb = mysql_escape_string($jb);
 	$doodle = mysql_escape_string($doodle);
 	$id_pertanyaan = mysql_escape_string($id_pertanyaan);
 	$id_user = mysql_escape_string($id_user);
--- template/question.php
+++ template/question.php
@@ -1,6 +1,8 @@
 <?php
 
 require_once __DIR__.'/../sql/question.php';
+require_once __DIR__.'/../sql/answer.php';
+require_once __DIR__.'/../sql/user.php';
 
 ?>
 <html>
@@ -26,7 +28,20 @@ require_once __DIR__.'/../sql/question.php';
 			<h3>
 			<?php
 				$data = getQuestionAnswerCommentById($_GET['id']);
+				$iduser = getUser($_SESSION["username"]);
 				echo $data['title'];
+				
+				if(isset($_POST["submit"]) && $_SESSION["status"] == 1)
+				{
+					$jb = $_POST["answer"];
+					$doodle = "";
+					$id_quest = $data["id"];
+					$id_user = $iduser["id"];
+					
+					postAnswer($jb, $doodle, $id_quest, $id_user);
+				}
+				else if(isset($_POST["submit"]) && $_SESSION["status"] != 1)
+					echo "Anda harus login terlebih dahulu agar dapat memposting jawaban";
 			?></h3>
 			<div id="contain">
 			<img src="../images/questions-and-answers.jpg" width="100"> <br>
