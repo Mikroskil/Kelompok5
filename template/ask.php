59617a5029e01f3143f6171b39d8dcf7fe603176
Hendra Wijaya <hendra.huang92@yahoo.com>
2013-11-28 15:50:46 +0700

    buat fitur doodle

--- template/ask.php
+++ template/ask.php
@@ -5,7 +5,9 @@
 <html>
 	<head>
 		<title>Asking</title>
+		<link rel="stylesheet" type="text/css" href="../assets/css/bootstrap.min.css" />
 		<link rel="stylesheet" type="text/css" href="../assets/css/global.css" />
+        <script type="text/javascript" src="../assets/js/jquery.min.js"></script>
 	</head>
 	<body>
 		<?php include_once 'header.php'; ?>
@@ -32,18 +34,28 @@
 				<label for="form-title">Title</label>
 				<input type="text" size="100%" name="title" id="form-title">
 				<br>
-				<a href="" name="doodle"><img src="../images/doodlechat.jpg" vspace="2"></a>
+                <a href="#" class="btn doodle-add">Add Doodle</a>
+                <a href="#" class="btn btn-danger doodle-cancel">Cancel</a>
+                <div class="doodle-container">
+                    <canvas id="doodle" width="800"></canvas>
+                    <div>
+                        <a href="#" class="btn btn-inverse btn-small pen">Pen</a>
+                        <a href="#" class="btn btn-inverse btn-small eraser">Eraser</a>
+                    </div>
+                </div>
 				<br>
 				<textarea name="quest" rows="10" cols="80%" id="form-quest" placeholder="Type your question here..."></textarea>
 				<br>
 				<label for="form-tags">Tags</label>
 				<input type="text" size="100%" name="tags" id="form-tags">
 				<br>
-				<input type="submit" name="submit" value="Post Your Question">
+				<input type="submit" name="submit" value="Post Your Question" class="btn btn-primary">
 			</div>
 			</form>
 		</div>
 		<br class="break" />
 		<?php include_once 'footer.html'; ?>
+        <script type="text/javascript" src="../assets/js/bootstrap.min.js"></script>
+        <script type="text/javascript" src="../assets/js/doodle.js"></script>
 	</body>
 </html>
